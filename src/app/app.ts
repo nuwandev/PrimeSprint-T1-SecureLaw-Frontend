39c2d6fa55a6e387ee89d1532db16d2b985c1e6c
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-03-08 14:51:27 +0530

    feat: add health service and environment configurations; update app component for backend health check

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { HealthService } from './services/health';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('SecureLaw');
+
+  constructor(private readonly healthService: HealthService) {}
+
+  ngOnInit(): void {
+    console.log(`Backend URL :${environment.apiUrl}`);
+
+    this.healthService.checkBackend().subscribe({
+      next: (res) => console.log('Backend is UP', res),
+      error: (err) => console.error('Backend is DOWN', err),
+    });
+  }
 }
