f0eb3fb90d134229fc29a44ad032720890cd288b
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-12 14:44:40 +0530

    implement Token service with methods to set, get, and remove token from localStorage

--- src/app/core/services/token.ts
+++ src/app/core/services/token.ts
@@ -5,4 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class Token {
   
+  setToken(token:string){
+    localStorage.setItem("token",token)
+  }
+
+  getToken(){
+    return localStorage.getItem("token")
+  }
+
+  removeToken(){
+    localStorage.removeItem("token")
+  }
 }
