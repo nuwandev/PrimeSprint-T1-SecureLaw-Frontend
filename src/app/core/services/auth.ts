f0c409a32a6ffebcf7fd1503f8f168b24987a705
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-03-15 10:10:36 +0530

    feat: update Auth service to use environment variables for API endpoints

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,27 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginRequest } from '../../models/auth';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
+  API = `${environment.apiUrl}/api/auth`;
 
-  API = "http://localhost:8080/api/auth";
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(data: LoginRequest) {
-    return this.http.post(`${this.API}/login`, data)
+    return this.http.post(`${this.API}/login`, data);
   }
 
   logout(data: any) {
-    return this.http.post(`${this.API}/logout`, data)
+    return this.http.post(`${this.API}/logout`, data);
   }
 
-  me(){
-  return this.http.get(
-    "http://localhost:8080/api/auth/me"
-  );
-}
+  me() {
+    return this.http.get(`${environment.apiUrl}/api/auth/me`);
+  }
 }
