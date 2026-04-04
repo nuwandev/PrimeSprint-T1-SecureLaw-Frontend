6305bdb2d1cbf223a87b07b1d35682963a7367e8
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-04-04 15:09:59 +0530

    Refactor forgot password component: update HTML structure

--- src/app/auth/login/forgotpassword/forgotpassword.ts
+++ src/app/auth/login/forgotpassword/forgotpassword.ts
@@ -1,29 +1,16 @@
+
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpassword',
-  imports: [],
   templateUrl: './forgotpassword.html',
   styleUrl: './forgotpassword.css',
+  imports: [CommonModule, FormsModule],
+  standalone: true,
 })
-
 export class Forgotpassword {
-  email='';
-  newPassword='';
-  
-   private baseUrl = 'http://localhost:8080/api/auth';
-
-  constructor(private http: HttpClient) {}
-
-  login(data: any) {
-    return this.http.post(`${this.baseUrl}/login`, data);
-  }
-
-  resetPassword(data: any) {
-    return this.http.post(`${this.baseUrl}/users/reset-password`, data);
-  }
-
+ 
 }
